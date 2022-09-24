87f536e8e136e3b553e238280a8bbf0a1a607210
ixap2i <ayamashita124@gmail.com>
2022-09-24 19:13:43 +0900

    update gitignore and app

--- application.go
+++ application.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/ixap2i/rss-app/rss"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		fmt.Fprintf(w, "Hello World! Append a name to the URL to say hello. For example, use %s/Mary to say hello to Mary.", r.Host)
+		rss()
 	} else {
 		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	}
